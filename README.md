4e1a6e4a8cdfa3121da097971bb3d12017055dce
Shivansh <ssgyanibaba@gmail.com>
2023-06-22 12:03:36 +0530

    'Initial_Commit'

---
+++ README.md
@@ -0,0 +1,7 @@
+To create virtual evironment
+    conda create -p enc python=3.10 -y
+
+To activate v.env (CMD)
+    conda activate C:\Users\Dell\Desktop\PROJECT\Wafer_fault\env
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Shivansh',
+    author_email='ssgyanibaba@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
