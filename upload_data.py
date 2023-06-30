fd3c56b25e08d9f61cfccddc594551f8c930083c
Shivansh <ssgyanibaba@gmail.com>
2023-06-30 10:06:46 +0530

    'Added_DataBase_Connecion'

---
+++ upload_data.py
@@ -0,0 +1,22 @@
+from pymongo.mongo_client import MongoClient
+import pandas as pd
+import json
+
+uri = "mongodb+srv://<User_Name>:<Password>@cluster0.yc3hy9y.mongodb.net/?retryWrites=true&w=majority"
+
+DataBase_Name='Shiv_expert'
+Collection_name='Wafer_faults'
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+#read data as csv
+df=pd.read_csv(r'C:\Users\Dell\Desktop\PROJECT\Wafer_fault\Notebook\wafer_23012020_041211.csv')
+df=df.drop('Unnamed: 0',axis=1)
+
+#converting dataframe to json so that we can load it in mongoDB
+json_records=list(json.loads(df.T.to_json()).values())
+
+
+#Dumping the data into the database
+client[DataBase_Name][Collection_name].insert_many(json_records)
